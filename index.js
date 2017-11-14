3eb71e912852de789d1926e89f6ec9232546ad6f
Sara Smith <sarasmith887@gmail.com>
2017-11-14 01:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+  function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+  }
+
+  function setbestCustomer() {
+    var bestCustomer = 'not bob';
+  }
+setbestCustomer()
